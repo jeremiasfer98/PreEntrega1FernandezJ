846563e59c7d91805f994eb827f1367c7d732abb
Jeremias <fernandez.jeremias23@gmail.com>
2023-09-27 13:28:56 -0300

    first commit

---
+++ src/cuerpo.jsx
@@ -0,0 +1,11 @@
+const Cuerpo = () => {
+    const nombre = "Jeremias"
+    return (
+        <main>
+            <h2>Home</h2>
+            <p>Bienvenido {nombre}! </p>
+        </main>
+
+    )
+}
+export default Cuerpo
